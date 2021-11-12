b67ef8945a2c73b20e357928c1f85988e88b7121
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-12 10:11:10 +0000

    2nd commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log(`Hello World`); 
+const prompt = require('prompt-sync')();
+
+var hascorrectopinion = false;
+var isbetter = prompt(`Is var better than let? Y/N: `);
+
+while (hascorrectopinion == false) {
+    if (isbetter != 'Y') {
+        isbetter = prompt(`Incorrect. Is var better than let? Y/N: `);
+        hascorrectopinion = false;
+    }
+    else if (isbetter == 'Y') {
+        console.log(`Correct.`)
+        hascorrectopinion = true;
+    }
+}
